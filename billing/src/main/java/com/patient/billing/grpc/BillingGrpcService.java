497af81eedc90f33130fdab07a49a6c728464ea8
Piyush <sab9861@gmail.com>
2026-09-01 00:13:21 +0530

    docker compose added

--- billing/src/main/java/com/patient/billing/grpc/BillingGrpcService.java
+++ billing/src/main/java/com/patient/billing/grpc/BillingGrpcService.java
@@ -22,5 +22,6 @@ public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBas
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+        log.info("recieved createBillingAccount request: {}",response.toString());
     }
 }
